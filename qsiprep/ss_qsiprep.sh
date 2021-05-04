69dfdd6445bde6a62ea79be9442412b8c3cf97e6
Steven Meisler <smeisler@g.harvard.edu>
2021-05-04 11:43:33 -0400

    Update ss_qsiprep.sh
    Cleaned up comments

--- qsiprep/ss_qsiprep.sh
+++ qsiprep/ss_qsiprep.sh
@@ -21,11 +21,9 @@ scratch=
 output_dir=${bids_dir}/derivatives
 
 mkdir -p ${scratch}
-# mkdir -p "${base}/derivatives"
 mkdir -p ${output_dir}
 
 cmd="singularity run -B ${scratch}:/workdir -B ${bids_dir}:/mnt:ro -B ${output_dir}:/output -B ${bids_dir}/code/qsiprep/license.txt:/license.txt $IMG --participant_label ${subject:4} -w /workdir --fs-license-file /license.txt --mem_mb 19000 --unringing_method mrdegibbs --skip-bids-validation --output_resolution 1.2 /mnt /output participant"
-#--use-syn-sdc --force-syn
 echo Submitted job for: ${subject}
 echo $'Command :\n'${cmd}
 
